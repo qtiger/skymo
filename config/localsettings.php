bc8529c024911b0b873c32a5d5b51bbce54250d8
ian <thequantumtiger@gmail.com>
2015-09-09 11:12:08 +0100

    Comments to describe the elements in the settings files

--- config/localsettings.php
+++ config/localsettings.php
@@ -1,5 +1,7 @@
 <?php
 require_once "config/settings.php";
 
+# Comment out or remove this line in production enviroments to supress
+# error messages being displayed to the user
 define("_DEBUG_",true);
 ?>
--- config/settings.php
+++ config/settings.php
@@ -1,9 +1,19 @@
 <?php
+# The following lines determine where skymo looks for its files
 define(_JSONDIR_,"core/json/");
 define(_CNTDIR_,"core/content/");
 define(_SCRDIR_,"core/scripts/");
 define(_TPLDIR_,"core/tpl/");
+
+# The following lines determine the file extensions that skymo files use
+# eg mytemplate.tpl
 define(_TPLEXT_,".tpl");
-define(_PAGETAG_,"pages");
 define(_CNTEXT_,".htm");
+define(_MDEXT_,".md");
+
+# The page tag determines the json tag that skymo looks for in the site
+# file for the list of web pages in the site
+define(_PAGETAG_,"pages");
+
+# The site file is the json file which contains the website structure
 $siteFile =_JSONDIR_ ."site.json"; ?>
