1e94f6e4ade1e6105b37eb5fb640dbd106863b1e
ian <thequantumtiger@gmail.com>
2015-09-10 20:23:44 +0100

    Added debug error if Json file not found

--- core/scripts/skymo.php
+++ core/scripts/skymo.php
@@ -3,7 +3,10 @@ class skymo {
 public static function getJson($f) {
   if (file_exists($f))
    return json_decode(file_get_contents($f),true);
-  else return null;
+  else {
+    if (_DEBUG_) trigger_error("File $f not found");
+    return null;
+    }
   }
 }
 ?>
