8d44d3c88fbe1dbd8586093c5d106f2910d0674b
Ian <thequantumtiger@gmail.com>
2016-06-10 06:41:59 +0100

    Dev: Remove uneeded code from adm.php

--- core/scripts/adm.php
+++ core/scripts/adm.php
@@ -1,37 +1,4 @@
 <?php
-$changes = false;
-
-/*function setfld($fld, $type) {
-  global $t, $changes;
-
-  if (array_key_exists($fld,$_POST)) {
-    if ($_POST[$fld] == "" && array_key_exists($fld, $t->sf[_PAGETAG_][$t->pgurl])) {
-      unset($t->sf[_PAGETAG_][$t->pgurl][$fld]);
-      $changes = true;
-      }
-    else {
-      $s = skymo::stringOK ($_POST[$fld], $type);
-      if ($s) {
-        $t->sf[_PAGETAG_][$t->pgurl][$fld] = $_POST[$fld];
-        $changes = true;
-        }
-      }
-    }
-  }
-
-function write() {
-    global $changes, $t, $siteFile;
-     
-    if ($changes) {
-      $t->msg="Changed";
-      $j = str_replace("},","},\n",json_encode($t->sf,128));
-      if ($j)
-        { $t->msg="Written";
-        file_put_contents ($siteFile, $j);
-        }
-      }
-    } */
-
 $t->msg = "Ready";
 if (array_key_exists(_ADMPAR_,$q)) {
   $t->sitepg = $t->sf[_PAGETAG_][$q[_ADMPAR_]];
@@ -46,11 +13,13 @@ if (array_key_exists(_ADMPAR_,$q)) {
       $changed = false;
       $t->msg="Posted";
       foreach ($t->flds as $fld) {
-        if (array_key_exists($fld["tag"], $t->sf[_PAGETAG_][$t->pgurl]))
-          $ret = skymo::setfld($fld["tag"],$fld["type"]);
+        if (array_key_exists($fld["tag"], $_POST))
+          $ret = skymo::setfld($fld["tag"], $_POST[$fld["tag"]], $fld["type"]);
           if ($ret) $changed = true;
         }
-      if ($changed) skymo::write();
+      if ($changed)
+        if (skymo::write())
+          $t->msg="Changes saved";;
       }
     }
   }
